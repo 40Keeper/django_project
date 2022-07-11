1cf63fc35caa00aa02466ee94356b9b0592cad20
40Keeper <aashman40000@gmail.com>
2022-07-11 16:48:08 +0530

    Added 3rd column for sensor

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -14,11 +14,15 @@ def home(request):
 
 #Gets executed when Run button is clicked
 def runbutton1(request):
-    exec(open("/home/nips/newproject/django_project/blog/python_scripts/testing.py").read())  
+    exec(open("/home/nips/newproject/django_project/blog/python_scripts/sshconnectiontest1.py").read())  
     return render(request,'blog/runpage.html')
 
 def runbutton2(request):
-    exec(open("/home/nips/newproject/django_project/blog/python_scripts/testing.py").read())
+    exec(open("/home/nips/newproject/django_project/blog/python_scripts/sshconnectiontest2.py").read())
+    return render(request,'blog/runpage.html')
+
+def runbutton3(request):
+    exec(open("/home/nips/newproject/django_project/blog/python_scripts/sshconnectiontest1_1.py").read())
     return render(request,'blog/runpage.html')
 
 def about(request):
